4d490366b124b2e13085d4cc3cffb0138ed024fc
Luc Weinbrecht <luc@weinbrecht.de>
2025-07-31 17:26:00 +0200

    WIP: started data jdbc test. started migration to data jdbc instead of jpa

--- backend/business-logic-modulith/bestellung/adapter/out/src/main/java/de/envite/greenbpm/schulung/referenzimplementierung/adapter/out/db/entity/BestellungEntity.java
+++ backend/business-logic-modulith/bestellung/adapter/out/src/main/java/de/envite/greenbpm/schulung/referenzimplementierung/adapter/out/db/entity/BestellungEntity.java
@@ -1,21 +1,21 @@
 package de.envite.greenbpm.schulung.referenzimplementierung.adapter.out.db.entity;
 
-import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
-@Entity
+// TODO: Data JDBC
+//@Entity
 public class BestellungEntity {
 
-  @OneToOne(cascade = CascadeType.ALL, optional = false, fetch = FetchType.LAZY)
-  @JoinColumn(name = "fahrzeug_id", referencedColumnName = "id")
+//  @OneToOne(cascade = CascadeType.ALL, optional = false, fetch = FetchType.LAZY)
+//  @JoinColumn(name = "fahrzeug_id", referencedColumnName = "id")
   FahrzeugEntity fahrzeug;
   Long antragstellerId;
   LocalDateTime bestelldatum;
   String status;
-  @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+//  @Id
+//  @GeneratedValue(strategy = GenerationType.SEQUENCE)
   private Long id;
 }
--- backend/business-logic-modulith/bestellung/adapter/out/src/main/java/de/envite/greenbpm/schulung/referenzimplementierung/adapter/out/db/entity/FahrzeugEntity.java
+++ backend/business-logic-modulith/bestellung/adapter/out/src/main/java/de/envite/greenbpm/schulung/referenzimplementierung/adapter/out/db/entity/FahrzeugEntity.java
@@ -1,18 +1,15 @@
 package de.envite.greenbpm.schulung.referenzimplementierung.adapter.out.db.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
-@Entity
+// TODO: Data JDBC
+//@Entity
 public class FahrzeugEntity {
   String hersteller;
   String modell;
   Integer jahr;
-  @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+//  @Id
+//  @GeneratedValue(strategy = GenerationType.SEQUENCE)
   private Long id;
 }

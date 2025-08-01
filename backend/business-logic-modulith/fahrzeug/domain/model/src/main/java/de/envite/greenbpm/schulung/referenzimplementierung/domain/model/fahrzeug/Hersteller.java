c8cb35fb6201a06e86879a90f6b959c649d361b6
Luc Weinbrecht <luc@weinbrecht.de>
2025-08-01 10:36:17 +0200

    added 'fahrzeug' module

---
+++ backend/business-logic-modulith/fahrzeug/domain/model/src/main/java/de/envite/greenbpm/schulung/referenzimplementierung/domain/model/fahrzeug/Hersteller.java
@@ -0,0 +1,12 @@
+package de.envite.greenbpm.schulung.referenzimplementierung.domain.model.fahrzeug;
+
+import io.github.domainprimitives.type.ValueObject;
+
+import static io.github.domainprimitives.validation.Constraints.hasMinLength;
+
+public class Hersteller extends ValueObject<String> {
+
+    public Hersteller(String value) {
+        super(value, hasMinLength(2));
+    }
+}

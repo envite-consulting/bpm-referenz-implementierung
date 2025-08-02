70512696cfd4dd7c3f056029959ce81d810edda0
SebastianRoseneck <55637012+SebastianRoseneck@users.noreply.github.com>
2025-08-02 09:22:41 +0200

    Remove fahrzeug and app from current branch

--- backend/business-logic-modulith/fahrzeug/adapter/out/src/main/java/de/envite/greenbpm/schulung/referenzimplementierung/fahrzeug/adapter/out/db/FahrzeugJdbcRepository.java
+++
@@ -1,8 +0,0 @@
-package de.envite.greenbpm.schulung.referenzimplementierung.fahrzeug.adapter.out.db;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-interface FahrzeugJdbcRepository extends CrudRepository<FahrzeugEntity, String> {
-}
--- backend/business-logic-modulith/fahrzeug/usecase/exception/src/main/java/de/envite/greenbpm/schulung/referenzimplementierung/fahrzeug/usecase/exception/FahrzeugNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package de.envite.greenbpm.schulung.referenzimplementierung.fahrzeug.usecase.exception;
-
-public class FahrzeugNotFoundException extends RuntimeException {
-  public FahrzeugNotFoundException(String message) {
-    super(message);
-  }
-}

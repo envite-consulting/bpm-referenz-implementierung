b42d90dc8b12aeed8706224395612e940211069e
Luc Weinbrecht <luc@weinbrecht.de>
2025-08-08 06:57:22 +0200

    immutable antragsteller id

--- backend/business-logic-modulith/antragsteller/domain/model/src/main/java/de/envite/greenbpm/schulung/referenzimplementierung/antragsteller/domain/model/Antragsteller.java
+++ backend/business-logic-modulith/antragsteller/domain/model/src/main/java/de/envite/greenbpm/schulung/referenzimplementierung/antragsteller/domain/model/Antragsteller.java
@@ -8,26 +8,22 @@ import lombok.Getter;
 @EqualsAndHashCode(callSuper = false)
 public class Antragsteller extends Aggregate {
 
-  private AntragstellerId antragstellerId;
+  private final AntragstellerId antragstellerId;
   private final Vorname vorname;
   private final Nachname nachname;
   private final Abteilung abteilung;
 
-  public Antragsteller(Vorname vorname, Nachname nachname, Abteilung abteilung) {
+  public Antragsteller(AntragstellerId antragstellerId, Vorname vorname, Nachname nachname, Abteilung abteilung) {
+    this.antragstellerId = antragstellerId;
     this.vorname = vorname;
     this.nachname = nachname;
     this.abteilung = abteilung;
     this.validate();
   }
 
-  public void setAntragstellerId(AntragstellerId antragstellerId) {
-    this.antragstellerId = antragstellerId;
-    validateNotNull(antragstellerId, "Antragsteller ID");
-    evaluateValidations();
-  }
-
   @Override
   protected void validate() {
+    validateNotNull(antragstellerId, "Antragsteller ID");
     validateNotNull(vorname, "Vorname");
     validateNotNull(nachname, "Nachname");
     validateNotNull(abteilung, "Abteilung");

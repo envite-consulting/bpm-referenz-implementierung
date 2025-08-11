6f6830b0df6a496e9c9921804d58a558910a8bd1
SebastianRoseneck <55637012+SebastianRoseneck@users.noreply.github.com>
2025-08-11 16:57:38 +0200

    Add SpringBootApplication test

---
+++ backend/business-logic-modulith/app/src/test/java/de/envite/greenbpm/schulung/referenzimplementierung/app/ApplicationTest.java
@@ -0,0 +1,13 @@
+package de.envite.greenbpm.schulung.referenzimplementierung.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+  @Test
+  void contextLoads() {
+    // empty
+  }
+}

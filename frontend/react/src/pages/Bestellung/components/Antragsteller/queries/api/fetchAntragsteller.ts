bc923ed20ddac6124180797a822089082dc06f92
SebastianRoseneck <55637012+SebastianRoseneck@users.noreply.github.com>
2025-08-12 17:36:04 +0200

    Add getAntragstellerOptions

--- frontend/react/src/pages/Bestellung/components/Antragsteller/queries/api/fetchAntragsteller.ts
+++ frontend/react/src/pages/Bestellung/components/Antragsteller/queries/api/fetchAntragsteller.ts
@@ -1,13 +1,31 @@
 import axios from 'axios';
 import { z } from 'zod';
-import { antragstellerSchema } from '@/pages/Bestellung/components/Antragsteller/Antragsteller.types.ts';
+import {
+  type Antragsteller,
+  antragstellerSchema,
+} from '@/pages/Bestellung/components/Antragsteller/Antragsteller.types.ts';
+import type { DropdownOption } from '@/infrastructure/components/DropDownMenu/DropdownMenu.tsx';
 
 export async function getAntragsteller() {
   try {
-    return z.array(antragstellerSchema).parse(response.data);
     const response = await axios.get('/api/antragsteller');
+    const antragstellerList: Antragsteller[] = z
+      .array(antragstellerSchema)
+      .parse(response.data);
+    return antragstellerList;
   } catch (error) {
     console.error(error);
     throw error;
   }
+}
+
+export async function getAntragstellerOptions(): Promise<
+  DropdownOption<string>[]
+> {
+  const antragstellerList = await getAntragsteller();
+
+  return antragstellerList.map((antragsteller) => ({
+    label: `${antragsteller.vorname} ${antragsteller.nachname} (${antragsteller.abteilung})`,
+    value: antragsteller.id,
+  }));
 }

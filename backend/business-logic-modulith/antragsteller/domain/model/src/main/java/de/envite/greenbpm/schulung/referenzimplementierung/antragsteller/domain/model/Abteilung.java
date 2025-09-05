c5e354cc32ede931282a67eb989d3f5c417807a1
Luc Weinbrecht <luc@weinbrecht.de>
2025-09-05 13:32:50 +0200

    improved arch tests

--- backend/business-logic-modulith/antragsteller/domain/model/src/main/java/de/envite/greenbpm/schulung/referenzimplementierung/antragsteller/domain/model/Abteilung.java
+++ backend/business-logic-modulith/antragsteller/domain/model/src/main/java/de/envite/greenbpm/schulung/referenzimplementierung/antragsteller/domain/model/Abteilung.java
@@ -1,9 +1,8 @@
 package de.envite.greenbpm.schulung.referenzimplementierung.antragsteller.domain.model;
 
-import static io.github.domainprimitives.validation.Constraints.hasMinLength;
-import static io.github.domainprimitives.validation.Constraints.isNotNull;
-
 import io.github.domainprimitives.type.ValueObject;
+
+import static io.github.domainprimitives.validation.Constraints.hasMinLength;
 
 public class Abteilung extends ValueObject<String> {
   public Abteilung(String value) {
--- backend/business-logic-modulith/app/src/test/java/de/envite/greenbpm/schulung/referenzimplementierung/app/CleanArchitectureTest.java
+++ backend/business-logic-modulith/app/src/test/java/de/envite/greenbpm/schulung/referenzimplementierung/app/CleanArchitectureTest.java
@@ -4,18 +4,21 @@ import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
+import org.apache.commons.lang3.ArrayUtils;
 
 import static com.tngtech.archunit.core.domain.JavaClass.Predicates.resideInAPackage;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
 @AnalyzeClasses(
-        packages = "de.envite.greenbpm.schulung.referenzimplementierung",
+        packages = CleanArchitectureTest.BASE_PACKAGE,
         importOptions = ImportOption.DoNotIncludeTests.class
 )
 class CleanArchitectureTest {
 
-    private static final String DOMAIN_SERVICES = "..domain.service..";
+    static final String BASE_PACKAGE = "de.envite.greenbpm.schulung.referenzimplementierung";
+
+    private static final String DOMAIN_SERVICE = "..domain.service..";
     private static final String DOMAIN_MODEL = "..domain.model..";
     private static final String USE_CASE = "..usecase..";
     private static final String USE_CASE_IN = "..usecase.in..";
@@ -24,10 +27,29 @@ class CleanArchitectureTest {
     private static final String ADAPTER_IN = "..adapter.in..";
     private static final String ADAPTER_OUT = "..adapter.out..";
 
+    /**
+     * Pakete, die nicht als Technologie angesehen werden und daher in der Domaine genutzt werden koennen
+     */
+    private static final String[] NON_TECHNOLOGY_PACKAGE = {
+            BASE_PACKAGE + "*.domain.model..",
+            "lombok..",
+            "java.lang..",
+            "java.time..",
+            "java.util..",
+            "io.github.domainprimitives.*."
+    };
+
+    /**
+     * Pakete, die nicht als Technologie angesehen werden und daher in der Domaine genutzt werden koennen
+     */
+    private static final String[] NON_TECHNOLOGY_PACKAGE_WITH_DEPENDENCY_INJECTION = ArrayUtils.addAll(
+            NON_TECHNOLOGY_PACKAGE,
+            "org.springframework.stereotype.*");
+
     @ArchTest
     static ArchRule domain_services_should_reside_in_domain_service_package = classes()
             .that().haveSimpleNameEndingWith("DomainService")
-            .should().resideInAPackage(DOMAIN_SERVICES);
+            .should().resideInAPackage(DOMAIN_SERVICE);
 
     @ArchTest
     static ArchRule mappers_should_reside_in_adapter = classes()
@@ -36,15 +58,20 @@ class CleanArchitectureTest {
 
     @ArchTest
     static ArchRule domain_services_should_implement_use_case_in = classes()
-            .that().resideInAPackage(DOMAIN_SERVICES)
+            .that().resideInAPackage(DOMAIN_SERVICE)
             .and().haveSimpleNameEndingWith("DomainService")
             .should().implement(resideInAPackage(USE_CASE_IN))
             .andShould().notImplement(resideInAPackage(USE_CASE_OUT));
 
     @ArchTest
     static ArchRule domain_services_should_not_use_adapters = noClasses()
-            .that().resideInAPackage(DOMAIN_SERVICES)
+            .that().resideInAPackage(DOMAIN_SERVICE)
             .should().accessClassesThat().resideInAPackage(ADAPTER);
+
+    @ArchTest
+    static ArchRule domain_services_should_not_use_technology = noClasses()
+            .that().resideInAPackage(DOMAIN_SERVICE)
+            .should().onlyAccessClassesThat().resideInAnyPackage(NON_TECHNOLOGY_PACKAGE_WITH_DEPENDENCY_INJECTION);
 
     @ArchTest
     static ArchRule domain_model_should_not_use_adapters = noClasses()
@@ -55,6 +82,11 @@ class CleanArchitectureTest {
     static ArchRule domain_model_should_not_use_use_cases = noClasses()
             .that().resideInAPackage(DOMAIN_MODEL)
             .should().accessClassesThat().resideInAPackage(USE_CASE);
+
+    @ArchTest
+    static ArchRule domain_model_should_not_use_technology = noClasses()
+            .that().resideInAPackage(DOMAIN_MODEL)
+            .should().onlyAccessClassesThat().resideInAnyPackage(NON_TECHNOLOGY_PACKAGE);
 
     @ArchTest
     static ArchRule adapter_in_should_not_use_adapter_out = noClasses()
